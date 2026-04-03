561d784b02d97b1598343c18724140111dccec46
Waleed Zaman <waleed.zaman@innosoft.company>
2026-04-03 14:46:14 +0500

    clean commit without secret

---
+++ modules/ec2/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "myserver" {
+  name   = "first-server"
+  region = "nyc1"
+  size   = "s-1vcpu-1gb-35gb-intel"
+  image  = "ubuntu-24-04-x64"
+  vpc_uuid = "5abf887a-ba3d-4a38-823d-ef7382a4d85d"
+
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> cae32ab (comminting all)
