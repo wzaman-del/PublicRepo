e68cb72478eec52ae7d72a030a5d1eda730fc97d
Waleed Zaman <waleed.zaman@innosoft.company>
2026-04-03 15:21:15 +0500

    chaning

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -16,9 +16,4 @@ resource "digitalocean_droplet" "myserver" {
   size   = "s-1vcpu-1gb-35gb-intel"
   image  = "ubuntu-24-04-x64"
   vpc_uuid = "5abf887a-ba3d-4a38-823d-ef7382a4d85d"
-
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> cae32ab (comminting all)
