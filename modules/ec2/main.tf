c1d610acd0c50878f8febdd9bf0de6ee376395aa
Waleed Zaman <waleed.zaman@innosoft.company>
2026-04-03 15:28:32 +0500

    chaningg

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -11,9 +11,10 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_droplet" "myserver" {
-  name   = "first-server"
+  name   = "DDeply-server"
   region = "nyc1"
   size   = "s-1vcpu-1gb-35gb-intel"
   image  = "ubuntu-24-04-x64"
   vpc_uuid = "5abf887a-ba3d-4a38-823d-ef7382a4d85d"
+
 }
