e0bd93093588f673d426268cb19b3e4b499a9c0d
Waleed Zaman <waleed.zaman@innosoft.company>
2026-04-06 18:26:19 +0500

    Import existing 3 DigitalOcean droplets into Terraform state

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -7,23 +7,33 @@ terraform {
 }
 
 provider "digitalocean" {
-  token = var.do_token
-}
+    token = var.do_token
+    }
 
-resource "digitalocean_droplet" "myserver" {
-  name   = "deply-server"
-  region = "nyc1"
-  size   = "s-1vcpu-1gb-35gb-intel"
-  image  = "ubuntu-24-04-x64"
-  vpc_uuid = "5abf887a-ba3d-4a38-823d-ef7382a4d85d"
-
-}
-
-resource "digitalocean_droplet" "server" {
+# Import resource 1
+resource "digitalocean_droplet" "first_server" {
   name   = "first-server"
   region = "nyc1"
   size   = "s-1vcpu-1gb-35gb-intel"
   image  = "ubuntu-24-04-x64"
   vpc_uuid = "5abf887a-ba3d-4a38-823d-ef7382a4d85d"
-
 }
+
+# Import resource 2
+resource "digitalocean_droplet" "deply_server" {
+  name   = "deply-server"
+  region = "nyc1"
+  size   = "s-1vcpu-1gb-35gb-intel"
+  image  = "ubuntu-24-04-x64"
+  vpc_uuid = "5abf887a-ba3d-4a38-823d-ef7382a4d85d"
+}
+
+# Import resource 3 (duplicate deply-server)
+resource "digitalocean_droplet" "deply_server_2" {
+  name   = "deply-server"
+  region = "nyc1"
+  size   = "s-1vcpu-1gb-35gb-intel"
+  image  = "ubuntu-24-04-x64"
+  vpc_uuid = "5abf887a-ba3d-4a38-823d-ef7382a4d85d"
+}
+
